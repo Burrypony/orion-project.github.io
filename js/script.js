9b82814ce88522503aa92988edb1d5d64d8e9a70
Kiril <kiril.chernysh@gmail.com>
2017-11-24 16:17:50 +0200

    its done v2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 $("documents").ready(function() {
-  if ($(window).width() < 480) {
-    $(".box__second-photo").find(".slick-track").addClass("w100per");
-  };
-
   $('.box__main-photo').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -18,28 +14,5 @@ $("documents").ready(function() {
     focusOnSelect: true,
 
   });
-  $(".box").on("click", ".slick-arrow", function() {
-    console.log("231456ewfwe");
-  });
-  $(".slick-next").click(function() {
-    $(".box__second-photo").find(".slick-track").css("transform", "translate3d(0px, 0px, 0px)");
-  });
-  $(".slick-prev").click(function() {
-    $(".box__second-photo").find(".slick-track").css("transform", "translate3d(0px, 0px, 0px)");
-  });
-  $(".box__main-photo").on('touchmove', function() {
-    $(".box__second-photo").find(".slick-track").css("transform", "translate3d(0px, 0px, 0px)");
-  });
-  $(".box__main-photo").on('slidesToScroll', function() {
-    $(".box__second-photo").find(".slick-track").css("transform", "translate3d(0px, 0px, 0px)");
-  });
-  $(".box__main-photo").on('animate', function() {
-    $(".box__second-photo").find(".slick-track").css("transform", "translate3d(0px, 0px, 0px)");
-  });
-  $(".box__main-photo").animate(function() {
-    $(".box__second-photo").find(".slick-track").css("transform", "translate3d(0px, 0px, 0px)");
-  });
-  // if (slick-track.style.transition = "transform500ms") {
-  //   $(".box__second-photo").find(".slick-track").css("transform", "translate3d(0px, 0px, 0px)");
-  // };
+
 });
